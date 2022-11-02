aa8c1eaba87e991aba5fc0170aeb79709b9de900
Daniela Alvarez Zegarra <daniela.alvarez04@gmail.com>
2022-11-02 03:27:34 +0000

    clean_merged finished

--- pipelines/merge_dataframes/merge_dataframes.py
+++ pipelines/merge_dataframes/merge_dataframes.py
@@ -2,37 +2,38 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
-expected_inputs = [[{
-    'email': ['aa','bb'],
-    'event_id': ['aa','bb'],
-    'attended_at': ['NaN','NaN']
-}],
+expected_inputs = [pd.DataFrame({
+    'email': ['aa','bb','cc'],
+    'event_id': ['1','2','2']
+}),
 
-[{
-    'id': ['aa', 'bb'],
+pd.DataFrame({
+    'id': ['1', '2'],
     'title': ['aa','bb'],
     'excerpt':['aa','bb'],
     'eventbrite_sync_description':['aa','bb'],
     'eventbrite_url':['aa','bb'],
     'eventbrite_id':['aa','bb'],
     'banner':['aa','bb']
-}]]
+})]
 
-expected_output = [{
-    'email': ['aa','bb'],
-    'event_id': ['aa','bb'],
-    'attended_at': ['NaN','NaN'],
-    'title': ['aa','bb']
-}]
+expected_output = pd.DataFrame({
+    'email': ['aa','bb','cc'],
+    'event_id': ['1','2','2'],
+    'title': ['aa','bb','bb']
+})
 
 
 def run(df,df2):
     """
     This function takes care of merging both datasets, events and attendies.
     """
-
+    print('Shape of df before merge', df.shape)
+    print('Shape of df2 before merge', df2.shape)
 
     merged_df = pd.merge(df, df2, left_on="event_id", right_on="id").drop(['id','excerpt',
                         'eventbrite_sync_description','eventbrite_url','eventbrite_id','banner'], axis=1)
+
+    print('Shape of merged_df', merged_df.shape)
 
     return merged_df
