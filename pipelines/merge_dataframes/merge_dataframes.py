9af335a7698359cd89a7b192d34416bdc0a68786
Charly Chacón <107764250+Charlytoc@users.noreply.github.com>
2024-11-28 07:57:38 -0500

    Update merge_dataframes.py

--- pipelines/merge_dataframes/merge_dataframes.py
+++ pipelines/merge_dataframes/merge_dataframes.py
@@ -29,29 +29,22 @@ def run(df, df2):
     """
     print('Shape of df before merge', df.shape)
     print('Shape of df2 before merge', df2.shape)
-    print("Merging dataframes")
-
-    merged_df = pd.merge(df, df2, left_on="event_id", right_on="id", how='left').drop(['id', 'excerpt',
-                        'eventbrite_sync_description', 'eventbrite_url', 'eventbrite_id', 'banner'], axis=1)
 
 
-    # Convert datetime fields
-    datetime_columns = ['starting_at', 'attended_at', 'form_created_at', 'won_at']
-    for field in datetime_columns:
-        if field in merged_df.columns:  # Check if the field exists in merged_df
-            merged_df[field] = pd.to_datetime(merged_df[field], errors="coerce")
-            if merged_df[field].dt.tz is None:
-                merged_df[field] = merged_df[field].dt.tz_localize('UTC', ambiguous='infer')
-            else:
-                merged_df[field] = merged_df[field].dt.tz_convert('UTC')
+    merged_df = pd.merge(df, df2, left_on="event_id", right_on="id", how='left').drop(['id','excerpt',
+                        'eventbrite_sync_description','eventbrite_url','eventbrite_id','banner'], axis=1)
+    
+    for field in ['starting_at', 'attended_at', 'form_created_at', 'won_at']:
+        merged_df[field] = pd.to_datetime(merged_df[field])
+        merged_df[field] = merged_df[field].dt.tz_localize('UTC', ambiguous='infer') if merged_df[field].dt.tz is None else merged_df[field].dt.tz_convert('UTC')
+        
 
-    print("Adding a new column")
     merged_df['is_new_registree'] = merged_df.groupby('email')['created_at'].transform(lambda x: x == x.min())
-
+    
     # Print only the datetime columns
     print("------------------------")
     print("Datetime columns in merged_df:")
     print("------------------------")
-    print(merged_df[datetime_columns].head())  # Display the first few rows of the datetime columns
+    print(merged_df[datetime_columns].head())
     
     return merged_df
